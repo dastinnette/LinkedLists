732602b88ea8ce0890d7ec9a8318b26dd2ecb22d
David Stinnette <dastinnette@gmail.com>
2015-07-21 08:53:20 -0600

    append method complete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,9 +4,22 @@ require 'pry'
 class LinkedList
 
   attr_accessor :head
-# append an element to the end of the list
-  def append
 
+  def initialize(node = nil)
+    @head = node
+
+  end
+# append an element to the end of the list
+  def append(node)
+    if @head == nil
+      @head = node
+    else
+    current = @head
+      until current.next_node == nil
+        current = current.next_node
+      end
+      current.next_node = node
+    end
   end
 # prepend an element at the beginning of the list
   def prepend(node)
@@ -18,12 +31,24 @@ class LinkedList
     end
   end
 # insert an element at an arbitrary position in the list
-  def insert(node)
-    
+  def insert(index, node)
+
   end
 # includes? gives back true or false whether the supplied value is in the list
+  def includes?
+
+  end
 # pop an element from the end of the list
+  def pop
+  end
 # count the number of elements in the list
+  def count(node)
+    count = 0
+    if
+    count += 1
+    else
+
+  end
 # return the head value at the beginning of the list
 # return the tail value at the end of the list
 # find_by_index find the value at a numeric position
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -4,6 +4,33 @@ require './lib/linked_list'
 
 class LinkedListTest < Minitest::Test
 
+  def test_empty_list_can_append_node
+    list = LinkedList.new
+    node = Node.new("data")
+    list.append(node)
+    assert_equal node, list.head
+  end
+
+  def test_list_with_one_node_can_append_second_node
+    list = LinkedList.new
+    node = Node.new("data1")
+    node2 = Node.new("data2")
+    list.append(node)
+    list.append(node2)
+    assert_equal node2, list.head.next_node
+  end
+
+  def test_list_with_two_nodes_can_append_third_node
+    list = LinkedList.new
+    node = Node.new("data1")
+    node2 = Node.new("data2")
+    node3 = Node.new("data3")
+    list.append(node)
+    list.append(node2)
+    list.append(node3)
+    assert_equal node3, list.head.next_node.next_node
+  end
+
   def test_prepend_node_to_list_beginning
     list = LinkedList.new
     node = Node.new("data")
@@ -19,5 +46,19 @@ class LinkedListTest < Minitest::Test
     list.prepend(node2)
     assert_equal node2, list.head
     assert_equal node2.next_node, node1
+  end
+
+  def test_insert_node_at_arbitrary_position
+    skip
+    list = LinkedList.new
+    node1 = Node.new("Thunderstruck")
+    node2 = Node.new("Back in Black")
+    node3 = Node.new("You Shook Me All Night Long")
+    list.prepend(node2)
+    list.prepend(node1)
+    list.insert(1, node3)
+    assert_equal list.head.data, "Thunderstruck"
+    assert_equal list.head.next_node.data, "You Shook Me All Night Long"
+
   end
 end
